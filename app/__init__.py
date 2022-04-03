c2d111fad020418f96c0ca98deff5f0aa0fc6456
Michael Klein <Michaelklein1997@gmail.com>
2022-04-03 12:47:32 -0500

    attempting to change procfile to push to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,14 @@
 from flask import Flask
 from app.routes import home, api
 from app.db import init_db
-from dotenv import load_dotenv
-from os import getenv
 from app.utils import filters
-
-load_dotenv()
 
 def create_app(test_config=None):
   # set up app config
   app = Flask(__name__, static_url_path='/', static_folder='static')
   app.url_map.strict_slashes = False
   app.config.from_mapping(
-    SECRET_KEY='SECRETSARENOFUNUNLESSYOUSHARETHEMWITHEVERYONE'
+    SECRET_KEY='super_secret_key'
   )
 
   app.jinja_env.filters['format_date'] = filters.format_date
